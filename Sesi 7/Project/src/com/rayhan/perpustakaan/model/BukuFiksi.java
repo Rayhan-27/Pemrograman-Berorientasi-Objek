63770d9c6850906cd28422ee4b864b197b4da2ef
Rayhan Sandika <rayhan.sandika_ti24@nusaputra.ac.id>
2026-04-25 16:10:56 +0700

    Upload Tugas

---
+++ Sesi 7/Project/src/com/rayhan/perpustakaan/model/BukuFiksi.java
@@ -0,0 +1,13 @@
+package com.rayhan.perpustakaan.model;
+
+public class BukuFiksi extends Buku {
+
+    public BukuFiksi(String id, String judul) {
+        super(id, judul);
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("[FIKSI] " + getId() + " - " + getJudul());
+    }
+}
---
+++ Sesi 7/Project/src/com/rayhan/perpustakaan/service/Perpustakaan.java
@@ -0,0 +1,44 @@
+package com.rayhan.perpustakaan.service;
+
+import com.rayhan.perpustakaan.model.Buku;
+import com.rayhan.perpustakaan.util.DataManager;
+import java.util.ArrayList;
+
+public class Perpustakaan implements Peminjaman {
+
+    private DataManager<Buku> daftarBuku = new DataManager<>();
+
+    public void tambahBuku(Buku buku) {
+        daftarBuku.tambahData(buku);
+    }
+        
+    public void tampilkanBuku() {
+        for (Buku b : daftarBuku.getAllData()) {
+            b.displayInfo();
+        }
+    }
+
+    @Override
+    public void pinjamBuku(String id) {
+        for (Buku b : daftarBuku.getAllData()) {
+            if (b.getId().equals(id) && !b.isDipinjam()) {
+                b.setDipinjam(true);
+                System.out.println("Buku berhasil dipinjam");
+                return;
+            }
+        }
+        System.out.println("Buku tidak tersedia");
+    }
+
+    @Override
+    public void kembalikanBuku(String id) {
+        for (Buku b : daftarBuku) {
+            if (b.getId().equals(id) && b.isDipinjam()) {
+                b.setDipinjam(false);
+                System.out.println("Buku berhasil dikembalikan");
+                return;
+            }
+        }
+        System.out.println("Buku tidak ditemukan");
+    }
+}
